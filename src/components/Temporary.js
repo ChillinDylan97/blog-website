0b900f4d886f4b95c67a7b71cd17c627ab54400f
ChillinDylan97 <dylangilbert601@gmail.com>
2024-04-27 21:11:19 -0500

    Nav Bar Test
    This is a test of the Nav Bar running inside the app

---
+++ src/components/Temporary.js
@@ -0,0 +1,55 @@
+
+//Header
+//  Title (Leads back to landing page)
+//  Nav buttons
+//      Home
+//      About Me
+//          Title
+//          Purpose of blog descriptionq
+//      Contact
+//          Title
+//          Name: First, Last
+//          Email
+//          Message
+//          Submit Button
+//Body
+//  Search Bar
+//  Post Preview
+//      Picture (Links to post)
+//      Title (Links to post)
+//      Date Posted (Clickable?)
+//      Article Preview that cuts off near the bottom with ...
+//      Tags (Clickable)
+//  Previous Post Preview
+//      Clickable arrows and page numbers
+//
+//Footer
+//  Subscribe to Newsletter (Submit Button)
+//  Social Media Links
+//  Copyright
+//PAGES
+//  Landing Page
+//  Home
+//  About Me
+//  Contact
+//  Search Contents Page
+//  Post Page
+//      Picture
+//      Title
+//      Date Posted (Clickable?)
+//      Article
+//      Tags (Clickable)
+//          MAYBE: Buttons to share articles to other social media platforms
+//      Comment Section
+//        Message Box
+//        Submit Button
+//        Previous Comments by poster
+//        Display Profile Pic
+//        Date Posted
+//        Likes and dislikes
+//  Tag Search Contents
+//
+//
+//
+//
+//
